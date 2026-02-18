4176c3145c67d2a37ab99000e3e58345987c5787
Pecsrik2243 <patr4k.nemeth@gmail.com>
2026-02-18 13:38:50 +0100

    Javítás
    hibák javítása és commitolás

--- main.cpp
+++ main.cpp
@@ -4,29 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    // HIBA: NELEMENTS nem definiálva - a konstans N_ELEMENTS (kis n helyett)
-    int *b = new int[NELEMENTS];
-    // HIBA: hiányzik a pontosvessző a cout utáni lezáráshoz
-    std::cout << '1-100 ertekek duplazasa'
-    // HIBA: üres a for ciklus feltétele - az i nem nő
-    for (int i = 0;)
+    int *b = new int[N_ELEMENTS];
+    std::cout << "1-100 ertekek duplazasa" << std::endl;
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
         b[i] = i * 2;
     }
-    // HIBA: az üres for feltételben az i nem nő, így végtelen ciklus vagy egyáltalán nem fut
-    for (int i = 0; i; i++)
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
-        // HIBA: hiányzik a pontosvessző és nincs b[i] kiírása
-        std::cout << "Ertek:"
+        std::cout << "Ertek: " << b[i] << std::endl;
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    // HIBA: az atlag nem inicializálva (hibás érték lesz)
-    int atlag;
-    // HIBA: vesszőt használtak pontosvessző helyett az i++ előtt, és az i < N_ELEMENTS után
-    for (int i = 0; i < N_ELEMENTS, i++)
+    int atlag = 0;
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
-        // HIBA: hiányzik a pontosvessző az += után
-        atlag += b[i]
+        atlag += b[i];
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
